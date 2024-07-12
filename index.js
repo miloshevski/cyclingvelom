2b46240be226187a5aa26dd0633710abd2b06b3d
physicsman <milosevskialeksandar18@gmail.com>
2024-07-12 23:06:26 +0200

    added history section

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $(".tab-link").click(function (event) {
-    event.preventDefault();
     $(".tab-link").removeClass("active");
     $(this).addClass("active");
   });
