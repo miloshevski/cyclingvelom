c43eb40c7ff9c87490373d6c069f50adc78dc8c8
physicsman <milosevskialeksandar18@gmail.com>
2024-09-11 22:33:02 +0200

    Jaka cimanja

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route for the main page
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Route for gallery page
+app.get("/gallery", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "gallery.html"));
+});
+
+// Route for results page
+app.get("/results", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "results.html"));
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
